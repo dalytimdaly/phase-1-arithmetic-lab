f31b87ee0af00355c30debd23fdfc6f60393ddfc
Tim <dalytimdaly@gmail.com>
2022-08-08 15:42:03 -0600

    commit tree 0d7a5da274f581b5922ea58d4a6cf562b826c2a8

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 62
+let num2 = 1
 
-// Write your code here
+let multiply = num1 * num2
+
+multiply
+
+let random = Math.floor(Math.random() + 1);
+
+random
+
+
+let num3 = 68
+let num4 = 8
+
+
+let mod = num3 % num4
+
+mod
+
+let max = Math.max(1, 2, 3, 20);
+
+max
